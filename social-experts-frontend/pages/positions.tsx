7c895e175145c51b9cec76e8a3bf3ae2aa0af8f1
Alcides Morales Guedes <Alcides.Morales@gmail.com>
2023-10-26 10:58:58 +0200

    feat: recover list of positions

--- social-experts-frontend/pages/positions.tsx
+++ social-experts-frontend/pages/positions.tsx
@@ -1,26 +1,52 @@
-import {useEffect, useState} from 'react'
+import {useState} from 'react'
 import type {NextPage} from 'next'
-import {sha256} from 'js-sha256'
 
 import WalletLoader from 'components/WalletLoader'
-import {useSigningClient} from 'contexts/client'
+import { JsonObject, SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate'
+import { Button } from 'antd'
 
 const Positions: NextPage = () => {
-  const {walletAddress, signingClient, coreumQueryClient} = useSigningClient()
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
-  const [classCreated, setClassCreated] = useState(false)
-  const [nftClassDescription, setNFTClassDescription] = useState('')
-  const [nfts, setNfts] = useState<{ classId: string; id: string, uri: string, uriHash: string, owner: string }[]>([])
-  const [transferID, setTransferID] = useState("")
-  const [recipientAddress, setRecipientAddress] = useState('')
+  const [loading, _setLoading] = useState(false)
 
+  const queryPositions = async () => {
+    const rpcEndpoint =
+      process.env.NEXT_PUBLIC_CHAIN_RPC_ENDPOINT ||
+      "https://full-node.testnet-1.coreum.dev:26657/";
+    const PUBLIC_CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID;
+    const offlineSigner = await (window as any).getOfflineSigner(
+      PUBLIC_CHAIN_ID,
+    );
+    if (!offlineSigner) {
+      alert("Please install keplr extension");
+    } else {
+      const accounts = await offlineSigner.getAccounts();
+      const walletAddress = accounts[0]?.address;
+      console.log(`Wallet: ${walletAddress}`);
+
+      const memo = "Social Experts - Create new profile";
+      const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "";
+
+      const client = await SigningCosmWasmClient.connectWithSigner(
+        rpcEndpoint,
+        offlineSigner,
+      );
+
+      let queryMsg : JsonObject = {"query_profile_list":{}} ;
+
+      const result = await client.queryContractSmart(contractAddress, queryMsg );
+
+      console.info("The query execution response:", JSON.stringify(result));
+    }
+  };
+
+  
   return (
     <WalletLoader loading={loading}>
         <div>
           <h1 className="text-3xl font-bold my-8">
             List of open positions to apply... work in progress
           </h1>
+          <Button onClick={queryPositions}>Query Positions</Button>
         </div>
     </WalletLoader>
   )
